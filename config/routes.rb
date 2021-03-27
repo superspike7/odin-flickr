397bfba6a2bb48589f853a5a3cce5613433ce2b1
Spike Vinz Cruz <spikevinz@gmail.com>
2021-03-27 10:09:27 +0800

    add staticpages controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/home', to: 'static_pages#index'
+  root 'static_pages#index'
 end
+
