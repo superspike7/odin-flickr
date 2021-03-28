5bfc4d00318a171673bd9beacacf75139c94d724
Spike Vinz Cruz <spikevinz@gmail.com>
2021-03-28 10:26:32 +0800

    complete app

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,14 @@
 class StaticPagesController < ApplicationController
+  require 'flickr'
+
   def index
+    flickr = Flickr.new
+    if @user_id = params[:user_id]
+      @photos = flickr.people.getPhotos :user_id => @user_id 
+      @photos.map { |p| flickr.photos.getInfo :photo_id => p.id } 
+    end
+    rescue Flickr::FailedResponse
+    flash.now[:alert] = 'Unable to find User ID!'
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/home', to: 'static_pages#index'
+  get '/search' => 'static_pages#search'
   root 'static_pages#index'
 end
 
